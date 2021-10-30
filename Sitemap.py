0bd6ed9924d266e7b2adba3b3fe9343fe128cd16
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2021-10-30 10:10:14 +0700

    Update files

--- Sitemap.py
+++ Sitemap.py
@@ -1,39 +1,57 @@
-import requests
 import os
+from termcolor import cprint
 
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from selenium import webdriver
 
 url = 'https://lamdong.gov.vn/SitePages/Home.aspx'
+max = 100
+extended = True  # used for get more urls from existing url in urls
 
+# get domain name
 main_domain = urlparse(url).netloc
 
+# open browser
 browser = webdriver.Edge(executable_path=os.path.join(
     os.getcwd(), 'webdrivers', 'msedgedriver.exe'))
 
-# wait for load fully webpage
-browser.get(url)
 
-soup = BeautifulSoup(browser.page_source, 'html.parser')
+def print_red(x): return cprint(x, 'red')
+
+
+def get_urls_in_webpage(url, urls=[]):
+    # wait for load fully webpage
+    browser.get(url)
+    # page_source into beautiful_soup
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    # get all <a href='...''> in current webpage
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        domain = urlparse(url).netloc
+        if url not in urls and main_domain == domain and '.aspx' in url and '/video/' not in url:
+            urls.append(url)
+            if len(urls) == max:
+                break
+    print_red(f'Đã lấy {len(urls)} URL')
+
+
+def export_to_file(urls: list):
+    data_folder = os.path.join(os.getcwd(), 'data')
+    output_file = os.path.join(data_folder, 'urls.txt')
+    with open(output_file, mode='w', encoding='utf8') as writer:
+        for url in urls:
+            writer.write(f'{url}\n')
+
 
 urls = []
-max = 100
-
-for link in soup.find_all('a'):
-    url = link.get('href')
-    domain = urlparse(url).netloc
-    if url not in urls and main_domain == domain:
-        if len(urls) <= max:
-            urls.append(url)
-            print(url)
-        else:
+get_urls_in_webpage(url, urls)
+if len(urls) < max:
+    for url in urls:
+        get_urls_in_webpage(url, urls)
+        if len(urls) == max:
             break
 
-data_folder = os.path.join(os.getcwd(), 'data')
-output_file = os.path.join(data_folder, 'urls.txt')
-with open(output_file, mode='w', encoding='utf8') as writer:
-    for url in urls:
-        writer.write(f'{url}\n')
+export_to_file(urls)
 
 browser.close()
