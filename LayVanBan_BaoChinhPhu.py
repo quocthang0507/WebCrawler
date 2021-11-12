3c205b56ba3666dc150b5f02eff6d3d1ba74eec9
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2021-11-12 20:21:37 +0700

    Update file

--- LayVanBan_BaoChinhPhu.py
+++ LayVanBan_BaoChinhPhu.py
@@ -31,19 +31,22 @@ def get_text(url: str):
 
     result = []
     if soup:
-        div = soup.find("div", {"class": "article-body"})
-        if div:
-            tags = [div.find_all('p'), div.find_all(
-                'div'), div.find_all('td'), div.find_all('th')]
-            for tag in tags:
-                for p in tag:
-                    if p and p.get_text().strip():
-                        sents = sentence_tokenize(p.get_text())
-                        for s in sents:
-                            result.append(s)
-            end_time = time.time()
-            print_blue(
-                f'Đã lấy văn bản trong {url}, trong {round(end_time - start_time, 2)} giây')
+        divs = [soup.find("div", {"class": "article-body"}),
+                soup.find("div", {"class": "article"}),
+                soup.find("div", {"class": "main-article"})]
+        for div in divs:
+            if div:
+                tags = [div.find_all('p'), div.find_all('div'),
+                        div.find_all('td'), div.find_all('th')]
+                for tag in tags:
+                    for p in tag:
+                        if p and p.get_text().strip():
+                            sents = sentence_tokenize(p.get_text())
+                            for s in sents:
+                                result.append(s)
+                end_time = time.time()
+                print_blue(
+                    f'Đã lấy văn bản trong {url}, trong {round(end_time - start_time, 2)} giây')
     else:
         end_time = time.time()
         print_red(
@@ -61,14 +64,15 @@ if __name__ == '__main__':
     data_folder = os.path.join(os.getcwd(), 'data')
     urls_file = os.path.join(data_folder, 'urls_baochinhphu.txt')
     output_text_file = os.path.join(
-        data_folder, 'crawled_sentences_baochinhphu.txt')
+        data_folder, 'crawled_sentences_baochinhphu_2.txt')
 
     with open(output_text_file, 'w', encoding='utf8') as writer:
         for url in read_urls(urls_file):
             sents = get_text(url)
             if len(sents) > 0:
                 for s in sents:
-                    writer.write(f'{s}\n')
+                    if s.strip():
+                        writer.write(f'{s}\n')
             else:
                 print_red(
                     f'Đã bỏ qua {url} vì không tìm thấy nội dung cần thu thập')
